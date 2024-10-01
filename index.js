bc4723cddb5b24e1c980ad044dfeead5cca15b17
Pradeepp456 <panchalpradeep001@gmail.com>
2024-10-01 07:28:54 +0000

    baiscs

---
+++ index.js
@@ -0,0 +1,11 @@
+let score = 33
+// console.log( typeof score);
+
+let b = null
+console.log(typeof b)
+
+let newNumberChange = Number(b)
+
+
+console.log(typeof newNumberChange)
+console.log(newNumberChange)
