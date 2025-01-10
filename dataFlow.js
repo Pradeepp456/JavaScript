d235e2953c1629ead28d7b6185bead68aa683d80
Pradeepp456 <panchalpradeep001@gmail.com>
2025-01-10 06:50:44 +0000

    New Learining

---
+++ dataFlow.js
@@ -0,0 +1,28 @@
+// // in this we will read about if else and other data flow techinique 
+
+// const a = 10
+// if (a == 9){
+//     console.log(`The value of a is qual to 10`)
+// }
+// else{
+//     console.log(`The value is not equal to 10`)
+// }
+
+// const newObj = {
+//     name : "pradeep",
+//     lastName : "panchal",
+//     age : 24,
+//     city : "bundi"
+// }
+// const newArry = ["shivam", "gorkhe",26 , "indore"]
+// console.log(typeof newObj)
+// console.log(newObj)
+
+// console.log(typeof newArry)
+// console.log(newArry)
+// // console.log(length.newObj)
+// console.log(Array.length[newArry])
+
+
+// console.log(Object.keys(newObj).length) 
+
